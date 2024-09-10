06a1e43c6a4f21647250a19515a5767a58d255f2
himanushi <himanushi@users.noreply.github.com>
2024-09-10 19:41:53 +0900

    GraphQLのミューテーションとクエリのコメントアウト

--- backend/app/graphql/graphql_schema.rb
+++ backend/app/graphql/graphql_schema.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class GraphqlSchema < GraphQL::Schema
-  mutation(::MutationObject)
-  query(::QueryObject)
+  # mutation(::MutationObject)
+  # query(::QueryObject)
 
   use GraphQL::Batch
 
